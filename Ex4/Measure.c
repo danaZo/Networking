b898bd5cde089fe41c62f4e3abb739ad51c4b167
noamv <noamvan10@gmail.com>
2021-12-17 14:41:00 +0200

    init

---
+++ Ex4/Measure.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <signal.h>
+
+
+
+int main(){
+
+
+    //create a socket
+    int soc, e;
+    soc = socket(AF_INET, SOCK_STREAM, 0);
+    if( soc < 0){
+        printf("Socket creation failed\n");
+        exit(1);
+    }
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(8001);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    e = bind(soc, (struct sockaddr*) &address, sizeof(address) );
+    if( e < 0){
+        printf("Binding failed \n");
+        exit(1);
+    }
+
+    char buf[256]; // for CC algorithm change
+
+    soc = socket(AF_INET, SOCK_STREAM, 0);
+    if (soc < 0)
+    {
+        perror("socket");
+    }
+
+    // Default CC algorithm is Cubic, we just make sure this is the correct now
+    // So there will be no issues with recieving from sender.
+
+    int length = sizeof(buf);
+    if (getsockopt(soc, IPPROTO_TCP, TCP_CONGESTION, buf, &length) != 0)
+    {
+        perror("getsockopt");
+        return -1;
+    }
+
+    length = sizeof(buf);
+    if (getsockopt(soc, IPPROTO_TCP, TCP_CONGESTION, buf, &length) != 0)
+    {
+        perror("getsockopt");
+        return -1;
+    }
+    printf("Congestion Control Strategy: %s\n", buf);
+
+
+
+
+    int senderSoc;
+    double totalTime = 0;
+
+
+
+    //5 times in the first CC
+    for(int i = 0 ; i < 5 ; i++){
+        e = listen(soc, 2);
+        if( e < 0){
+            printf("Listening Failure \n");
+        }
+
+        senderSoc = accept(soc, NULL , NULL);
+        if( senderSoc < 0){
+        perror("Accepting Failure: ");
+    }
+
+        char buffer[1024];
+        int KbytesRec = 0 ;
+        clock_t start = clock();
+        while((e = recv(senderSoc, &buffer , sizeof(buffer) ,0) > 0)){
+            
+            if( e < 0){
+                perror("Error: ");
+          }
+            KbytesRec += e;
+        }
+        clock_t end = clock();
+        totalTime += (float)(end - start);
+        printf("%d\n", KbytesRec);
+
+        // printf("Message recieved: %s \n", buffer);
+        bzero(buffer, 1024);
+        close(senderSoc);
+    } 
+
+    printf("Total receiving time: %f seconds\n", totalTime/1000000);
+    printf("Average receiving time: %f seconds\n", totalTime/5000000);   
+
+    close(soc);
+
+    return 0;
+}
---
+++ Ex4/sender.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+
+void send_file(FILE *f, int socket){
+
+    char s[1024];
+    size_t nread;
+    while ((nread = fread(s, 1, sizeof s, f)) > 0){
+       int t = send(socket, s, sizeof(s), 0);
+            if(t < 0){
+                perror("sending failed \n");
+                exit(1);
+            }
+    }
+    if (ferror(f)) {
+        perror("Error: ");
+    }
+    printf("Message sent \n");
+}
+
+int main(){
+
+    FILE *f;
+    char *filename = "1mb.txt";
+
+    //create a socket
+    int soc;
+    
+    
+    
+    struct sockaddr_in measure_addr;
+    measure_addr.sin_family = AF_INET;
+    measure_addr.sin_port = htons(8001);
+    measure_addr.sin_addr.s_addr = INADDR_ANY;
+
+    int con;
+    float totalTime = 0;
+
+    for(int i = 0 ; i <5 ; i++){
+        soc = socket(AF_INET, SOCK_STREAM, 0);
+        con = connect(soc, (struct  sockaddr*) &measure_addr, sizeof(measure_addr));
+
+        if(con < 0 ){
+            perror("connection failed");
+        }
+        else{
+            printf("connection established successfuly\n");
+        }
+        
+        f = fopen(filename, "r");
+        if(!f){
+
+            printf("~ file opening failed ~");
+        }
+        clock_t start = clock();
+        send_file(f, soc);
+        clock_t end = clock();
+        totalTime += (float)(end - start);
+        close(soc);
+        fclose(f);
+            
+    }
+    printf("Total sending time: %f seconds\n", totalTime/1000000);
+    printf("Average sending time: %f seconds\n", totalTime/5000000);
+    
+    
+
+}
